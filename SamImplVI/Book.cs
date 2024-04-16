fcf67efa72aed3ede0c2f7b5b4b9ab29d1d4c152
davids1176 <1176@student.itgymnazium.cz>
2024-04-16 11:22:36 +0200

    Init

---
+++ SamImplVI/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SamImplVI
+{
+    public class Book
+    {
+        private string _author;
+        private string _name;
+        private int _year;
+        private int _pages;
+
+        public string Author { get { return _author; } }
+        public string Name { get { return _name; } }
+        public int Year { get { return _year; } }
+        public int Pages { get { return _pages; } }
+
+        public Book(string author, string name, int year, int pages)
+        {
+            _author = author;
+            _name = name;
+            _year = year;
+            _pages = pages;
+        }
+    }
+}
---
+++ SamImplVI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SamImplVI
+{
+    class MainClass
+    {
+        private static string[] authorNames = { "a", "b", "c" };
+        private static string[] bookNames = { "a", "b", "c", "d", "e", "f" };
+
+        public static void Main(string[] args)
+        {
+            Book[] books = new Book[10];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                Book book = new Book(authorNames[rand.Next(0, authorNames.Length)], bookNames[rand.Next(0, bookNames.Length)], rand.Next(0, 2024), rand.Next(0, 10000));
+                books[i] = book;
+            }
+
+            long total = 0;
+            foreach (Book book in books)
+            {
+                total += book.Pages;
+            }
+            double averagePages = (double)total / books.Length;
+            Console.WriteLine($"Průměrný počet stránek: {averagePages}");
+
+            Book maxPagesBook = books[0];
+            foreach (Book book in books)
+            {
+                if (book.Pages > maxPagesBook.Pages)
+                {
+                    maxPagesBook = book;
+                }
+            }
+            Console.WriteLine($"Jméno knihy s největším počtem stránek: {maxPagesBook.Name}");
+        }
+    }
+}
